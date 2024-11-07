8f9fd3e9a9dc6a07ff8db416104cfb3188a79982
paodac <paodac.cbc@gmail.com>
2024-11-07 14:37:13 -0600

    mirece change from png to jpeg

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import miRece from '../assets/projects/miRece.png'
+import miRece from '../assets/projects/miRece.jpeg'
 import taskid from '../assets/projects/taskid.jpeg'
 import blog from '../assets/projects/blog.jpeg'
 
